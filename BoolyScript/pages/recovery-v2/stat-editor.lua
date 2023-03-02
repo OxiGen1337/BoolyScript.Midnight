0a43638aada1be52fee7128977bcc3bf955ae30f
pNoName1337 <54634728+pNoName1337@users.noreply.github.com>
2023-03-02 22:15:39 +0300

    fix2

--- BoolyScript/pages/recovery-v2/stat-editor.lua
+++ BoolyScript/pages/recovery-v2/stat-editor.lua
@@ -17,18 +17,14 @@ ST:add_separator("Int", "BS_StatEditor_IntSeparator")
 
 local statNameI = ST:add_text_input("Name", "BS_StatEditor_IntName"):setConfigIgnore()
 
-local iValue = 0
-
 local INT_MAX = 2147483647
 
 local INT_MIN = -2147483647
 
-ST:add_num_option("Value", "BS_StatEditor_IntValue", INT_MIN, INT_MAX, 1, function (val)
-    iValue = val
-end):setValue(0, true):setConfigIgnore()
+local iValue = ST:add_text_input("Value", "BS_StatEditor_IntValue"):setConfigIgnore()
 
 ST:add_click_option("Set", "BS_StatEditor_IntSet", function(option)
-    STATS.STAT_SET_INT(string.joaat(tostring(statNameI)), iValue, true)
+    STATS.STAT_SET_INT(string.joaat(tostring(statNameI)), tonumber(iValue), true)
     STATS.STAT_SAVE(0, 0, 3, 0)
 end)
 --11
