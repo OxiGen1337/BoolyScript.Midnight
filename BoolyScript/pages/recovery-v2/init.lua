4ec1b934bfa515636f2301e618a5415cd8af6445
pNoName1337 <54634728+pNoName1337@users.noreply.github.com>
2023-03-02 22:12:43 +0300

    fix

--- BoolyScript/pages/recovery-v2/init.lua
+++ BoolyScript/pages/recovery-v2/init.lua
@@ -3,7 +3,7 @@ require("BoolyScript/util/DrawUI")
 V2 = Submenu.add_static_submenu("Recovery V2", "BS_Recovery_V2")
 HOME_SUBMENU:add_sub_option("Recovery V2", "BS_Recovery_V2", V2):setTags({{"[New]", 255, 5, 88}}):setHint("V2 options are risky and may lead to account suspension")
 ST = Submenu.add_static_submenu("Stat Editor", "BS_StatEditor")
-HOME_SUBMENU:add_sub_option("Stat Editor", "BS_StatEditor", V2):setTags({{"[Beta]", 20, 217, 197}})
+HOME_SUBMENU:add_sub_option("Stat Editor", "BS_StatEditor", ST):setTags({{"[Beta]", 20, 217, 197}})
 
 task.executeAsScript("Load_RecoveryV2", function ()
     require("BoolyScript/pages/recovery-v2/include")
