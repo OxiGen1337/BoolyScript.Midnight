7edb254e998c55866e0e6a3f201c530f2cfbc97a
pNoName1337 <54634728+pNoName1337@users.noreply.github.com>
2023-03-04 13:17:56 +0300

    mp01 part 1/2

--- BoolyScript/pages/DrawUI_Recovery/stat-editor.lua
+++ BoolyScript/pages/DrawUI_Recovery/stat-editor.lua
@@ -1,3 +1,9 @@
+local sMP, iMP = "MP", 0 -- TODO: char ID 
+
+ST:add_bool_option("Add MP0_/MP1_", "BS_StatEditor_MP01", function (state, option)
+    if state then sMP = "MP" .. iMP .. "_" else sMP = "" end
+end):setValue(false):setConfigIgnore()
+
 ST:add_separator("Bool", "BS_StatEditor_BoolSeparator")
 
 local statNameB = ST:add_text_input("Name", "BS_StatEditor_BoolName"):setConfigIgnore()
@@ -9,7 +15,7 @@ ST:add_choose_option("Value", "BS_StatEditor_BoolValue", true, {"FALSE", "TRUE"}
 end):setConfigIgnore()
 
 ST:add_click_option("Set", "BS_StatEditor_BoolSet", function(option)
-    STATS.STAT_SET_BOOL(string.joaat(tostring(statNameB:getValue())), bValue, true)
+    STATS.STAT_SET_BOOL(string.joaat(sMP .. tostring(statNameB:getValue())), bValue, true)
     STATS.STAT_SAVE(0, 0, 3, 0)
 end)
 
@@ -20,7 +26,7 @@ local statNameI = ST:add_text_input("Name", "BS_StatEditor_IntName"):setConfigIg
 local iValue = ST:add_text_input("Value", "BS_StatEditor_IntValue"):setValue("1"):setConfigIgnore()
 
 ST:add_click_option("Set", "BS_StatEditor_IntSet", function(option)
-    STATS.STAT_SET_INT(string.joaat(tostring(statNameI:getValue())), tonumber(iValue:getValue()), true)
+    STATS.STAT_SET_INT(string.joaat(sMP .. tostring(statNameI:getValue())), tonumber(iValue:getValue()), true)
     STATS.STAT_SAVE(0, 0, 3, 0)
 end)
 
@@ -31,6 +37,6 @@ local statNameF = ST:add_text_input("Name", "BS_StatEditor_FloatName"):setConfig
 local fValue = ST:add_text_input("Value", "BS_StatEditor_FloatValue"):setValue("1.0"):setConfigIgnore()
 
 ST:add_click_option("Set", "BS_StatEditor_FloatSet", function(option)
-    STATS.STAT_SET_FLOAT(string.joaat(tostring(statNameF:getValue())), s2f(fValue:getValue()), true)
+    STATS.STAT_SET_FLOAT(string.joaat(sMP .. tostring(statNameF:getValue())), s2f(fValue:getValue()), true)
     STATS.STAT_SAVE(0, 0, 3, 0)
 end)
