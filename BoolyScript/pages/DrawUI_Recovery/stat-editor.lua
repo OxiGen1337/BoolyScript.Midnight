f11d4d6de3a811f1f01f2202370bc60df6ec7d6c
pNoName1337 <54634728+pNoName1337@users.noreply.github.com>
2023-03-04 13:18:34 +0300

    fix

--- BoolyScript/pages/DrawUI_Recovery/stat-editor.lua
+++ BoolyScript/pages/DrawUI_Recovery/stat-editor.lua
@@ -1,4 +1,4 @@
-local sMP, iMP = "MP", 0 -- TODO: char ID 
+local sMP, iMP = "", 0 -- TODO: char ID 
 
 ST:add_bool_option("Add MP0_/MP1_", "BS_StatEditor_MP01", function (state, option)
     if state then sMP = "MP" .. iMP .. "_" else sMP = "" end
