cbef5531a2ad95f06b09ce751d95510de66a84ea
pNoName1337 <54634728+Props69420@users.noreply.github.com>
2023-03-11 16:01:37 +0300

    ordered table + cleanup

--- BoolyScript/pages/DrawUI_Recovery/main.lua
+++ BoolyScript/pages/DrawUI_Recovery/main.lua
@@ -28,28 +28,30 @@ V2:add_click_option("Auto Shop Scam", "BS_RecoveryV2_AS_Scam", function(option)
     notify.important("Real Estate Scam", "Now purchase another Auto Shop and replace it")
 end):setTags({{"[Risky]", 252, 43, 85}}):setHint("Purchase an Auto Shop -> Activate this option -> Purchase another Auto Shop and replace it")
 
-V2:add_separator("Unlocks", "BS_RecoveryV2_Unl")
+V2:add_separator("Unlocks", "BS_RecoveryV2_Unlocks")
 
-local unlockOptions = {
-    ["Extra"] = v2r.unlockExtra,
-    ["Clothes"] = v2r.unlockClothes,
-    ["Rockstar Clothes"] = v2r.unlockAdminStuff,
-    ["Tattoos"] = v2r.unlockTattoos,
-    ["Weapons"] = v2r.unlockWeapons,
-    ["Exclusive Content"] = v2r.unlockExclusiveContent,
-    ["Challenges"] = v2r.unlockChallenges,
-    ["Fast Run + Reload"] = v2r.unlockFastRunPlusReload,
-    ["Vehicle Mods"] = v2r.unlockVehicleMods,
-    ["Xmas Stuff"] = v2r.unlockXmas,
-    ["Valentines"] = v2r.unlockValentines,
-    ["Movie Props"] = v2r.unlockMovieProps,
-    ["Pilot School"] = v2r.unlockPilotSchool,
-    ["Property Access"] = v2r.unlockPropertyAccess,
-    ["Objectives"] = v2r.unlockObjectives,
+
+local unlockOptions = ordered_table.new{
+    "Extra", v2r.unlockExtra,
+    "Clothes", v2r.unlockClothes,
+    "Rockstar Clothes", v2r.unlockAdminStuff,
+    "Tattoos", v2r.unlockTattoos,
+    "Weapons", v2r.unlockWeapons,
+    "Exclusive Content", v2r.unlockExclusiveContent,
+    "Challenges", v2r.unlockChallenges,
+    "Fast Run + Reload", v2r.unlockFastRunPlusReload,
+    "Vehicle Mods", v2r.unlockVehicleMods,
+    "Xmas Stuff", v2r.unlockXmas,
+    "Valentines", v2r.unlockValentines,
+    "Movie Props", v2r.unlockMovieProps,
+    "Pilot School", v2r.unlockPilotSchool,
+    "Property Access", v2r.unlockPropertyAccess,
+    "Objectives", v2r.unlockObjectives
 }
 
+
 for name, func in pairs(unlockOptions) do
-    V2:add_click_option(name, "BS_RecoveryV2_" .. name, function(option)
+    V2:add_click_option(name, "BS_RecoveryV2_Unlocks_" .. name, function(option)
         func(c)
         option:setTags({{"[Unlocked]", 10, 245, 18}})
         notify.success("Unlock " .. name, "Success")
@@ -58,33 +60,35 @@ end
 
 V2:add_separator("Vehicles", "BS_RecoveryV2_Vehicles")
 
-V2:add_click_option("Shotaro", "BS_RecoveryV2_SHT", function(option)
-    v2r.unlockShotaro(c)
-    option:setTags({{"[Now Available]", 210, 255, 48}})
-    notify.success("Unlock Shotaro", "Success")
-end)
+local unlockVehs = ordered_table.new{
+    "Shotaro", v2r.unlockShotaro,
+    "Armored Paragon", v2r.unlockArmoredParagon
+}
 
-V2:add_click_option("Armored Paragon", "BS_RecoveryV2_ARP", function(option)
-    v2r.unlockArmoredParagon(c)
-    option:setTags({{"[Now Available]", 210, 255, 48}})
-    notify.success("Unlock Armored Paragon", "Success")
-end)
+for name, func in pairs(unlockVehs) do
+    V2:add_click_option(name, "BS_RecoveryV2_Vehicles_" .. name, function(option)
+        func(c)
+        option:setTags({{"[Now Available]", 210, 255, 48}})
+        notify.success("Unlock " .. name, "Success")
+    end)
+end
 
 V2:add_separator("Stats", "BS_RecoveryV2_Stats")
 
-V2:add_click_option("Awards", "BS_RecoveryV2_AWD", function(option)
-    v2r.awards(c)
-    option:setTags({{"[Unlocked]", 247, 72, 171}})
-    notify.success("Unlock Awards", "Success")
-end)
+local unlockStats = ordered_table.new{
+    "Awards", v2r.awards,
+    "Health", v2r.health
+}
 
-V2:add_click_option("Health", "BS_RecoveryV2_HLT", function(option)
-    v2r.health(c)
-    option:setTags({{"[Modified]", 247, 72, 171}})
-    notify.success("Modify Health", "Switch session to apply")
-end)
+for name, func in pairs(unlockStats) do
+    V2:add_click_option(name, "BS_RecoveryV2_Stats_" .. name, function(option)
+        func(c)
+        option:setTags({{"[Unlocked]", 247, 72, 171}})
+        notify.success("Unlock " .. name, "Success")
+    end)
+end
 
-V2:add_choose_option("Permanent Musket", "BS_RecoveryV2_MUS", false, {"Give", "Remove"}, function (value, option)
+--[[V2:add_choose_option("Permanent Musket", "BS_RecoveryV2_MUS", false, {"Give", "Remove"}, function (value, option)
     if value == 1 then
         v2r.giveMusket(c)
         option:setTags({{"[Given]", 70, 234, 242}})
@@ -94,7 +98,7 @@ V2:add_choose_option("Permanent Musket", "BS_RecoveryV2_MUS", false, {"Give", "R
         option:setTags({{"[Removed]", 245, 26, 10}})
         notify.success("Remove Musket", "Success")
     end
-end)
+end)]]
 
 V2:add_choose_option("Permanent Firework Launcher", "BS_RecoveryV2_FIR", false, {"Give", "Remove"}, function (value, option)
     if value == 1 then
--- BoolyScript/rage/features.lua
+++ BoolyScript/rage/features.lua
@@ -280,4 +280,79 @@ function switch(value_any, table_t, default_f)
 	return pcall(table_t[value_any] or default_f or function () return end)
 end
 
+ordered_table = {}
+
+--[[
+	t - table
+	k - key
+	v - value
+]]
+
+function ordered_table.insert(t, k, v)
+  if not rawget(t._values, k) then -- new key 
+    t._keys[#t._keys + 1] = k
+  end
+  if v == nil then -- delete key
+    ordered_table.remove(t, k)
+  else -- update/store value
+    t._values[k] = v 
+  end
+end
+
+local function find(t, value)
+  for i,v in ipairs(t) do
+    if v == value then
+      return i
+    end
+  end
+end  
+
+function ordered_table.remove(t, k)
+  local v = t._values[k]
+  if v ~= nil then
+    table.remove(t._keys, find(t._keys, k))
+    t._values[k] = nil
+  end
+  return v
+end
+
+function ordered_table.index(t, k)
+    return rawget(t._values, k)
+end
+
+function ordered_table.pairs(t)
+  local i = 0
+  return function()
+    i = i + 1
+    local key = t._keys[i]
+    if key ~= nil then
+      return key, t._values[key]
+    end
+  end
+end
+
+function ordered_table.new(init)
+  init = init or {}
+  local t = {_keys={}, _values={}}
+  local n = #init
+  if n % 2 ~= 0 then
+    return log.error("Ordered table", "Key is missing a value")
+  end
+  for i=1,n/2 do
+    local k = init[i * 2 - 1]
+    local v = init[i * 2]
+    if t._values[k] ~= nil then
+      log.error("Ordered table", "Duplicate key: " .. k)
+    end
+    t._keys[#t._keys + 1]  = k
+    t._values[k] = v
+  end
+  return setmetatable(t,
+    {__newindex=ordered_table.insert,
+    __len=function(t) return #t._keys end,
+    __pairs=ordered_table.pairs,
+    __index=t._values
+    })
+end
+
 return features
